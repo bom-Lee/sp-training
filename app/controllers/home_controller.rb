7e739477a51edd45aa3543559d5a999c4167dbf4
bom-lee <spring6292@gmail.com>
2022-12-16 01:51:03 +0900

    [#8] Add university & researcher crud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def index
     @notices = Notice.all
     @notice = Notice.new
+
+    @universities = University.all
+    @university = University.new
+
+    @researchers = Researcher.all
+    @researcher = Researcher.new
   end
 end
--- app/controllers/university_controller.rb
+++
@@ -1,6 +0,0 @@
-class UniversityController < ApplicationController
-  def index
-    @notices = Post.all
-    @notice = Post.new
-  end
-end
